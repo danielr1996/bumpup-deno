cf66f96ab782f9bd9bbcbf2aa62a74c7641bc05c
Daniel Richter <danielrichter@posteo.de>
2021-02-13 03:05:12 +0100

    BREAKING CHANGE: WIP
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- bumpup.config.ts
+++ bumpup.config.ts
@@ -1,9 +1,9 @@
-import {BumpupConfig} from "./packages/cli/src/lib/types.ts";
-import type from "./packages/type-git-conventional-changelog/type.ts";
-import version from "./packages/read-txt/version.ts";
-import determine from "./packages/determine-semver/determine.ts";
-import write from "./packages/write-txt/version.ts";
-import record from "./packages/type-git-conventional-changelog/record.ts";
+import {BumpupConfig} from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/cli/src/lib/types.ts";
+import type from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/type.ts";
+import version from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/read-txt/read.ts";
+import determine from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/determine-semver/determine.ts";
+import write from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/write-txt/write.ts";
+import record from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/record.ts";
 
 const config: BumpupConfig = {
     version: "2.0.0",
