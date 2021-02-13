9c9fc913bc98e66585a60ebfd7a1e38084726283
Daniel Richter <danielrichter@posteo.de>
2021-02-13 04:40:32 +0100

    fix: WIP
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

---
+++ bumpup.config.ts
@@ -0,0 +1,23 @@
+// import {BumpupConfig} from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/cli/src/lib/types.ts";
+import type
+    from "./packages/type-git-conventional-changelog/src/type.ts";
+import read from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/read-txt/src/read.ts";
+import determine
+    from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/determine-semver/src/determine.ts";
+import write from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/write-txt/src/write.ts";
+import record from "./packages/type-git-conventional-changelog/src/record.ts";
+// import record
+//     from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/src/record.ts";
+// import {BumpupConfig} from "./packages/cli/src/lib/types.ts";
+
+const config = {
+    version: "2.0.0",
+    plugins: [
+        read,
+        [type, {tagPrefix: 'main'}],
+        determine,
+        write,
+        [record, {tagPrefix: 'main'}],
+    ]
+};
+export default config;
