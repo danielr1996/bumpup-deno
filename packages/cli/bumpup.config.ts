894027b610612347825b6a1a07335a852c298944
Daniel Richter <danielrichter@posteo.de>
2021-02-24 23:01:42 +0100

    feat: add something
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- packages/cli/bumpup.config.ts
+++ packages/cli/bumpup.config.ts
@@ -1,9 +1,9 @@
-import read from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/read-txt/src/read.ts";
-import type from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/src/type.ts";
-import determine from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/determine-semver/src/determine.ts";
-import write from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/write-txt/src/write.ts";
-import record from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/src/record.ts";
-import {BumpupConfig} from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/cli/src/lib/types.ts";
+import read from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/feature/ci/packages/read-txt/src/read.ts";
+import type from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/feature/ci/packages/type-git-conventional-changelog/src/type.ts";
+import determine from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/feature/ci/packages/determine-semver/src/determine.ts";
+import write from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/feature/ci/packages/write-txt/src/write.ts";
+import record from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/feature/ci/packages/type-git-conventional-changelog/src/record.ts";
+import {BumpupConfig} from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/feature/ci/packages/cli/src/lib/types.ts";
 
 const config: BumpupConfig = {
     version: "2.0.0",
