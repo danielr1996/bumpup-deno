694e05ed29c4f7e8578ea954fbd704964f8761e8
Daniel Richter <danielrichter@posteo.de>
2021-02-24 22:33:03 +0100

    feat: add something
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

---
+++ packages/cli/bumpup.config.ts
@@ -0,0 +1,18 @@
+import read from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/read-txt/src/read.ts";
+import type from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/src/type.ts";
+import determine from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/determine-semver/src/determine.ts";
+import write from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/write-txt/src/write.ts";
+import record from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/type-git-conventional-changelog/src/record.ts";
+import {BumpupConfig} from "https://raw.githubusercontent.com/danielr1996/bumpup-deno/main/packages/cli/src/lib/types.ts";
+
+const config: BumpupConfig = {
+    version: "2.0.0",
+    plugins: [
+        read,
+        type,
+        determine,
+        write,
+        record,
+    ]
+};
+export default config;
